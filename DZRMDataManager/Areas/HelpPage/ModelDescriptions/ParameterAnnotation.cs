8741c010e7d2cf2faef8bb6da15d3000e02fe1b9
dzeleniak <dzeleniak0@gmail.com>
2020-07-12 17:51:04 -0700

    Add the Web API Project
    Adds a blank Web API Project

---
+++ DZRMDataManager/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DZRMDataManager.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
