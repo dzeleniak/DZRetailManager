ef104f20382bb41b1d79e384468f1fe284e6e4a0
dzeleniak <dzeleniak0@gmail.com>
2020-07-14 14:36:49 -0700

    Create Login View/Viewmodel
    Created a page for loggin into the application
    with a form that accepts username/password.

--- DZRMDesktopUI/Bootstrapper.cs
+++ DZRMDesktopUI/Bootstrapper.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using DZRMDesktopUI.Helpers;
 using DZRMDesktopUI.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace DZRMDesktopUI
 {
@@ -17,6 +19,12 @@ namespace DZRMDesktopUI
         public Bootstrapper()
         {
             Initialize();
+
+            ConventionManager.AddElementConvention<PasswordBox>(
+                PasswordBoxHelper.BoundPasswordProperty,
+                "Password",
+                "PasswordChanged"
+            );
         }
 
         protected override void Configure()
