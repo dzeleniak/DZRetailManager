f3ef22c7649b7a83b58cba6d3f69dea2a54b7458
dzeleniak <dzeleniak0@gmail.com>
2020-07-17 21:52:08 -0700

    Start Process of Getting User Data

---
+++ DZRMDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,25 @@
+﻿using DZRMDataManager.Library.Internal.DataAccess;
+using DZRMDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZRMDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string Id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var p = new { Id = Id };
+
+            var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "DefaultConnection");
+
+            return output;
+        }
+    }
+}
---
+++ DZRMDataManager/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using DZRMDataManager.Library.DataAccess;
+using DZRMDataManager.Library.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace DZRMDataManager.Controllers
+{
+
+    [Authorize]
+    public class UserController : ApiController
+    {
+
+        public List<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+            return data.GetUserById(userId); 
+        }
+    }
+}
